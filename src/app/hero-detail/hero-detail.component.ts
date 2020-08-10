dbf8460790542fa46cc90c795d4b2d4da31b034e
Gabriel C <gabriel@winterwind.com>
2020-08-10 10:39:03 +0800

    + finished tutorial

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router'
+import {Location} from '@angular/common'
+import {HeroService} from '../hero.service'
+import {Hero} from '../hero'
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+    hero: Hero;
+  constructor(
+        private activatedRoute: ActivatedRoute,
+        private heroService: HeroService,
+        private location: Location
+    ) { }
+
+  ngOnInit(): void {
+    this.getHero()
+  }
+  save():void{
+    this.heroService.updateHero(this.hero)
+        .subscribe(_=>this.goBack())
+  }
+  getHero(): void{
+    const id = +this.activatedRoute.snapshot.paramMap.get('id')
+    this.heroService.getHero(id)
+        .subscribe(hero=>this.hero = hero)
+  }
+  goBack():void{
+    this.location.back()
+  }
+}
